7f400a7be6395e0bf1025a36798b3423363123db
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-12-27 00:28:09 +0300

    register request model implemented.

---
+++ user_management/Dtos/RegisterRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace user_management.Dtos
+{
+    public class RegisterRequest
+    {
+        [Required,EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+        [Required,DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+        [Required,DataType(DataType.Password), Compare(nameof(Password), ErrorMessage = "Passwords don't match")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
+
