0ed2c01f92d1b8fd38fe61142f72d610ad658b1f
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-12-27 00:50:44 +0300

    response classes added.

---
+++ user_management/Dtos/Responses/LoginResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace user_management.Dtos.Responses
+{
+    public class LoginResponse
+    {
+        public bool Success { get; set; }
+        public string AccessToken { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
+
---
+++ user_management/Dtos/Responses/RegisterResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace user_management.Dtos.Responses
+{
+    public class RegisterResponse
+    {
+        public string Message { get; set; } = string.Empty;
+        public bool Success { get; set; }
+    }
+}
+
