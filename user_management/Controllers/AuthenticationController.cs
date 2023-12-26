25daa1a3e8ed9177f19a4a789ff75eabc722dc2d
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-12-27 00:44:39 +0300

    authentication controller added.

---
+++ user_management/Controllers/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace user_management.Controllers
+{
+    [ApiController]
+    [Route("api/v1/authenticate")]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        [ProducesResponseType((int)HttpStatusCode.OK, typeof(LoginResponse))]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+
+        }
+    }
+}
+
